e0ace1dff24a68ac7e184141456e887381c98e77
Your Name <amit.pinaki@gmail.com>
2019-07-29 08:52:52 +0530

    Spatial Bin

---
+++ 23_Object_Detection/016_spatial_bin/solution.py
@@ -0,0 +1,21 @@
+# Define a function to compute color histogram features  
+# Pass the color_space flag as 3-letter all caps string
+# like 'HSV' or 'LUV' etc.
+def bin_spatial(img, color_space='RGB', size=(32, 32)):
+    # Convert image to new color space (if specified)
+    if color_space != 'RGB':
+        if color_space == 'HSV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        elif color_space == 'LUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+        elif color_space == 'HLS':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        elif color_space == 'YUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+        elif color_space == 'YCrCb':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    else: feature_image = np.copy(img)             
+    # Use cv2.resize().ravel() to create the feature vector
+    features = cv2.resize(feature_image, size).ravel() 
+    # Return the feature vector
+    return features
---
+++ 23_Object_Detection/016_spatial_bin/spatial_bin.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# Read in an image
+# You can also read cutout2, 3, 4 etc. to see other examples
+image = mpimg.imread('cutout1.jpg')
+
+# Define a function to compute color histogram features  
+# Pass the color_space flag as 3-letter all caps string
+# like 'HSV' or 'LUV' etc.
+# KEEP IN MIND IF YOU DECIDE TO USE THIS FUNCTION LATER
+# IN YOUR PROJECT THAT IF YOU READ THE IMAGE WITH 
+# cv2.imread() INSTEAD YOU START WITH BGR COLOR!
+def bin_spatial(img, color_space='RGB', size=(32, 32)):
+    # Convert image to new color space (if specified)
+    # Use cv2.resize().ravel() to create the feature vector
+    features = img.ravel() # Remove this line!
+    # Return the feature vector
+    return features
+    
+feature_vec = bin_spatial(image, color_space='RGB', size=(32, 32))
+
+# Plot features
+plt.plot(feature_vec)
+plt.title('Spatially Binned Features')
