d8605dd7c4eca58d212e06da0a2b8d650ba1dc32
Your Name <amit.pinaki@gmail.com>
2020-02-18 08:51:49 +0530

    HOG update

--- 05_Object_Detection/016_spatial_bin/spatial_bin.py
+++ 05_Object_Detection/016_spatial_bin/spatial_bin.py
@@ -7,19 +7,36 @@ import matplotlib.image as mpimg
 # You can also read cutout2, 3, 4 etc. to see other examples
 image = mpimg.imread('cutout1.jpg')
 
-# Define a function to compute color histogram features  
+
+# Define a function to compute color histogram features
 # Pass the color_space flag as 3-letter all caps string
 # like 'HSV' or 'LUV' etc.
 # KEEP IN MIND IF YOU DECIDE TO USE THIS FUNCTION LATER
-# IN YOUR PROJECT THAT IF YOU READ THE IMAGE WITH 
+# IN YOUR PROJECT THAT IF YOU READ THE IMAGE WITH
 # cv2.imread() INSTEAD YOU START WITH BGR COLOR!
 def bin_spatial(img, color_space='RGB', size=(32, 32)):
-    # Convert image to new color space (if specified)
-    # Use cv2.resize().ravel() to create the feature vector
-    features = img.ravel() # Remove this line!
-    # Return the feature vector
-    return features
-    
+  # Convert image to new color space (if specified)
+  # Use cv2.resize().ravel() to create the feature vector
+  # Convert image to new color space (if specified)
+  if color_space != 'RGB':
+    if color_space == 'HSV':
+      feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    elif color_space == 'LUV':
+      feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+    elif color_space == 'HLS':
+      feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    elif color_space == 'YUV':
+      feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    elif color_space == 'YCrCb':
+      feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+  else:
+    feature_image = np.copy(img)
+  # Use cv2.resize().ravel() to create the feature vector
+  features = cv2.resize(feature_image, size).ravel()
+  # Return the feature vector
+  return features
+
+
 feature_vec = bin_spatial(image, color_space='RGB', size=(32, 32))
 
 # Plot features
--- 05_Object_Detection/017_car_notcar/car_notcar.py
+++ 05_Object_Detection/017_car_notcar/car_notcar.py
@@ -12,41 +12,42 @@ cars = []
 notcars = []
 
 for image in images:
-    if 'image' in image or 'extra' in image:
-        notcars.append(image)
-    else:
-        cars.append(image)
-        
-# Define a function to return some characteristics of the dataset 
+  if 'image' in image or 'extra' in image:
+    notcars.append(image)
+  else:
+    cars.append(image)
+
+
+# Define a function to return some characteristics of the dataset
 def data_look(car_list, notcar_list):
-    data_dict = {}
-    # Define a key in data_dict "n_cars" and store the number of car images
-    data_dict["n_cars"] = 0
-    # Define a key "n_notcars" and store the number of notcar images
-    data_dict["n_notcars"] = 0
-    # Read in a test image, either car or notcar
-    # Define a key "image_shape" and store the test image shape 3-tuple
-    data_dict["image_shape"] = (0, 0, 0)
-    # Define a key "data_type" and store the data type of the test image.
-    data_dict["data_type"] = None
-    # Return data_dict
-    return data_dict
-    
+  data_dict = {}
+  # Define a key in data_dict "n_cars" and store the number of car images
+  data_dict["n_cars"] = len(car_list)
+  # Define a key "n_notcars" and store the number of notcar images
+  data_dict["n_notcars"] = len(notcar_list)
+  # Read in a test image, either car or notcar
+  example_img = mpimg.imread(car_list[0])
+  # Define a key "image_shape" and store the test image shape 3-tuple
+  data_dict["image_shape"] = example_img.shape
+  # Define a key "data_type" and store the data type of the test image.
+  data_dict["data_type"] = example_img.dtype
+  # Return data_dict
+  return data_dict
+
+
 data_info = data_look(cars, notcars)
 
-print('Your function returned a count of', 
-      data_info["n_cars"], ' cars and', 
+print('Your function returned a count of', data_info["n_cars"], ' cars and',
       data_info["n_notcars"], ' non-cars')
-print('of size: ',data_info["image_shape"], ' and data type:', 
+print('of size: ', data_info["image_shape"], ' and data type:',
       data_info["data_type"])
-# Just for fun choose random car / not-car indices and plot example images   
+# Just for fun choose random car / not-car indices and plot example images
 car_ind = np.random.randint(0, len(cars))
 notcar_ind = np.random.randint(0, len(notcars))
-    
+
 # Read in car / not-car images
 car_image = mpimg.imread(cars[car_ind])
 notcar_image = mpimg.imread(notcars[notcar_ind])
-
 
 # Plot the examples
 fig = plt.figure()
