3b58bc3c6ef62db2a6826b201f44016d1de1c098
Your Name <amit.pinaki@gmail.com>
2020-02-19 06:34:51 +0530

    HOG features update

--- 05_Object_Detection/020_hog/get_hog.py
+++ 05_Object_Detection/020_hog/get_hog.py
@@ -7,17 +7,42 @@ from skimage.feature import hog
 
 # Read in our vehicles
 car_images = glob.glob('*.jpeg')
-        
+
 # Define a function to return HOG features and visualization
 # Features will always be the first element of the return
 # Image data will be returned as the second element if visualize= True
 # Otherwise there is no second return element
 
-def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=True, 
+
+def get_hog_features(img,
+                     orient,
+                     pix_per_cell,
+                     cell_per_block,
+                     vis=True,
                      feature_vec=True):
-                         
-    # TODO: Complete the function body and returns
-    pass
+  """
+    Function accepts params and returns HOG features (optionally flattened) and an optional matrix for
+    visualization. Features will always be the first return (flattened if feature_vector= True).
+    A visualization matrix will be the second return if visualize = True.
+    """
+
+  return_list = hog(img,
+                    orientations=orient,
+                    pixels_per_cell=(pix_per_cell, pix_per_cell),
+                    cells_per_block=(cell_per_block, cell_per_block),
+                    block_norm='L2-Hys',
+                    transform_sqrt=False,
+                    visualise=vis,
+                    feature_vector=feature_vec)
+
+  # name returns explicitly
+  hog_features = return_list[0]
+  if vis:
+    hog_image = return_list[1]
+    return hog_features, hog_image
+  else:
+    return hog_features
+
 
 # Generate a random index to look at a car image
 ind = np.random.randint(0, len(car_images))
@@ -26,10 +51,12 @@ image = mpimg.imread(car_images[ind])
 gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
 # Call our function with vis=True to see an image output
-features, hog_image = get_hog_features(gray, orient= 9, 
-                        pix_per_cell= 8, cell_per_block= 2, 
-                        vis=True, feature_vec=False)
-
+features, hog_image = get_hog_features(gray,
+                                       orient=9,
+                                       pix_per_cell=8,
+                                       cell_per_block=2,
+                                       vis=True,
+                                       feature_vec=False)
 
 # Plot the examples
 fig = plt.figure()
